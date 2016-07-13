0f7f12bf03d831686d413aeb0deea7bf4920e4c6
DaryaKolyadko <skvoznak1996@gmail.com>
2016-07-14 01:44:21 +0300

    Update aws_iot_config.h

--- RobotIotApp/aws_iot_config.h
+++ RobotIotApp/aws_iot_config.h
@@ -1,18 +1,3 @@
-/*
- * Copyright 2010-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 /**
  * @file aws_iot_config.h
  * @brief AWS IoT specific configuration file
@@ -26,7 +11,7 @@
 #define AWS_IOT_MQTT_HOST              "a28raxclt7zm5c.iot.us-west-2.amazonaws.com" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
 #define AWS_IOT_MQTT_PORT              8883 ///< default port for MQTT/S
 #define AWS_IOT_MQTT_CLIENT_ID         "KolyadkoThing" ///< MQTT client ID should be unique for every device
-#define AWS_IOT_MY_THING_NAME 	 	   "KolyadkoThing" ///< Thing Name of the Shadow this device is associated with
+#define AWS_IOT_MY_THING_NAME 	 	     "KolyadkoThing" ///< Thing Name of the Shadow this device is associated with
 #define AWS_IOT_ROOT_CA_FILENAME       "root.pem.crt" ///< Root CA file name
 #define AWS_IOT_CERTIFICATE_FILENAME   "8210489b5e-certificate.pem.crt" ///< device signed certificate file name
 #define AWS_IOT_PRIVATE_KEY_FILENAME   "8210489b5e-private.pem.key" ///< Device private key filename
